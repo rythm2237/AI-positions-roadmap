bb965e4b067f5b3f52979d8ef245740eb8a5959f
Yaser Tayyebialashti <95166759+rythm2237@users.noreply.github.com>
2026-08-03 08:18:56 +0200

    feat: add optional GTM and GA4 loader

---
+++ src/components/analytics/AnalyticsScripts.tsx
@@ -0,0 +1,44 @@
+import Script from "next/script";
+
+export default function AnalyticsScripts() {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+  if (process.env.NODE_ENV !== "production") return null;
+
+  if (gtmId) {
+    return (
+      <>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+      </>
+    );
+  }
+
+  if (gaId) {
+    return (
+      <>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;gtag('js',new Date());gtag('config','${gaId}',{anonymize_ip:true,send_page_view:true});`}
+        </Script>
+      </>
+    );
+  }
+
+  return null;
+}
